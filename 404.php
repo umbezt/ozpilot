af8d91a100e37b01b60f2108b5d3300acad89f6a
Tapiwa Maumbe <tapiwamaumbe@gmail.com>
2016-11-02 14:43:24 +0200

    Update 404.php

--- 404.php
+++ 404.php
@@ -4,5 +4,11 @@
  */
 
 get_header(); ?>
-
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12">
+      <h1>404  <small>sorry we couldn't find your the you looking for.</small></h1>
+    </div>
+  </div>
+</div>
 <?php get_footer(); ?>
