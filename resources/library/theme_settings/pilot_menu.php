e47a93ec058cfc672099087b71098db811c1cc45
Tapiwa Maumbe <tapiwamaumbe@gmail.com>
2016-11-02 15:30:47 +0200

    Adding fields to the theme settings page

--- resources/library/theme_settings/pilot_menu.php
+++ resources/library/theme_settings/pilot_menu.php
@@ -1,6 +1,20 @@
 <?php
 
-function pilot_settings_page(){}
+function pilot_settings_page(){
+	?>
+	    <div class="wrap">
+	    <h1>Theme Panel</h1>
+	    <form method="post" action="options.php">
+	        <?php
+	            settings_fields("section");
+	            do_settings_sections("theme-options");      
+	            submit_button(); 
+	        ?>          
+	    </form>
+		</div>
+	<?php
+
+}
 
 function add_pilot_menu_item()
 {
@@ -8,3 +22,58 @@ function add_pilot_menu_item()
 }
 
 add_action("admin_menu", "add_pilot_menu_item");
+function display_twitter_element()
+{
+	?>
+    	<input type="text" name="twitter_url" id="twitter_url" value="<?php echo get_option('twitter_url'); ?>" />
+    <?php
+}
+
+function display_facebook_element()
+{
+	?>
+    	<input type="text" name="facebook_url" id="facebook_url" value="<?php echo get_option('facebook_url'); ?>" />
+    <?php
+}
+
+function display_layout_element()
+{
+	?>
+		<input type="checkbox" name="theme_layout" value="1" <?php checked(1, get_option('theme_layout'), true); ?> /> 
+	<?php
+}
+function logo_display()
+{
+	?>
+        <input type="file" name="logo" /> 
+        <?php echo get_option('logo'); ?>
+   <?php
+}
+
+function handle_logo_upload()
+{
+	if(!empty($_FILES["demo-file"]["tmp_name"]))
+	{
+		$urls = wp_handle_upload($_FILES["logo"], array('test_form' => FALSE));
+	    $temp = $urls["url"];
+	    return $temp;   
+	}
+	  
+	return $option;
+}
+function display_theme_panel_fields()
+{
+    add_settings_section("section", "All Settings", null, "theme-options");
+	
+    add_settings_field("twitter_url", "Twitter Profile Url", "display_twitter_element", "theme-options", "section");
+    add_settings_field("facebook_url", "Facebook Profile Url", "display_facebook_element", "theme-options", "section");
+    add_settings_field("theme_layout", "Do you want the layout to be responsive?", "display_layout_element", "theme-options", "section");
+    add_settings_field("logo", "Logo", "logo_display", "theme-options", "section");  
+
+    register_setting("section", "logo", "handle_logo_upload");
+    register_setting("section", "twitter_url");
+    register_setting("section", "facebook_url");
+    register_setting("section", "theme_layout");
+}
+
+add_action("admin_init", "display_theme_panel_fields");
