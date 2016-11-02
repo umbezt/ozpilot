879ef664a06dca99b83b9be7c823a7946d57e2df
Tapiwa Maumbe <tapiwamaumbe@gmail.com>
2016-11-02 13:42:49 +0200

    Create section.php

---
+++ resources/library/PilotDefaults/ShortCodes/section.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Usage: [section_container class="custom-class"]Go here[/section_container]
+ * Attribute Class: btn is added by default. Just add any other classes.
+ * 
+ */
+function section_container_shortcode( $attributes, $content = null ) {
+	$atts = shortcode_atts( [
+		'class' => '',		
+	], $attributes );
+
+	return '<section class="' . $atts['class'] . '" > <div class="container" >' . do_shortcode($content) . '</div></section>';
+}
+add_shortcode( 'section_container', 'section_container_shortcode' );
+
+/**
+ * Usage: [section_container class="custom-class"]Go here[/section_container]
+ * Attribute Class: btn is added by default. Just add any other classes.
+ * 
+ */
+function section_fullwidth_shortcode( $attributes, $content = null ) {
+	$atts = shortcode_atts( [
+		'class' => '',		
+	], $attributes );
+
+	return '<section class="' . $atts['class'] . '" > <div class="container-fluid" >' . do_shortcode($content) . '</div></section>';
+}
+add_shortcode( 'section_fullwidth', 'section_fullwidth_shortcode' );
